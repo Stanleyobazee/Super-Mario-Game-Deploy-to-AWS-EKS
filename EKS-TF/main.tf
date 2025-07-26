76d7dd9960e9e6ece99ff72d86c8f1aba1ffffb3
Stanleyobazee <stanleyobazee@gmail.com>
2025-07-26 15:48:16 +0200

    update main.tf

--- EKS-TF/main.tf
+++ EKS-TF/main.tf
@@ -5,48 +5,15 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-  tags = {
-    Name = "${var.project_name}-vpc"
-  }
+data "aws_vpc" "default" {
+  default = true
 }
 
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "${var.project_name}-igw"
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
   }
-}
-
-resource "aws_subnet" "public" {
-  count             = 2
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.${count.index + 1}.0/24"
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "${var.project_name}-public-subnet-${count.index + 1}"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-  tags = {
-    Name = "${var.project_name}-public-rt"
-  }
-}
-
-resource "aws_route_table_association" "public" {
-  count          = 2
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
 }
 
 resource "random_id" "cluster_suffix" {
@@ -113,7 +80,7 @@ resource "aws_eks_cluster" "main" {
   name     = "${var.project_name}-cluster-${random_id.cluster_name_suffix.hex}"
   role_arn = aws_iam_role.eks_cluster.arn
   vpc_config {
-    subnet_ids = aws_subnet.public[*].id
+    subnet_ids = data.aws_subnets.default.ids
   }
   depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy]
   tags = {
@@ -127,7 +94,7 @@ resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "${var.project_name}-node-group"
   node_role_arn   = aws_iam_role.eks_node_group.arn
-  subnet_ids      = aws_subnet.public[*].id
+  subnet_ids      = data.aws_subnets.default.ids
 
   scaling_config {
     desired_size = 1
